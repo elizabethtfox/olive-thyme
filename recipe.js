2fd18dcadaadd2cd2bf597dc0b39b81e7fd8174e
Elizabeth Fox <elizabethtfox93@gmail.com>
2018-05-30 12:45:52 -0400

    updated models

--- recipe.js
+++ recipe.js
@@ -1,1 +1,28 @@
-//test
+module.exports = function(sequelize, DataTypes) {
+    var Recipe = sequelize.define("Recipe", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        }
+    });
+
+    Recipe.associate = function(models) {
+        // We're saying that a Recipe should belong to a Category
+        // A Recipe can't be created without an Category due to the foreign key constraint
+        Recipe.belongsTo(models.Category, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Recipe;
+};
