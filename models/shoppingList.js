4129ded6baf66a09a82d26068e81ab0dd10d730d
Elizabeth Fox <elizabethtfox93@gmail.com>
2018-06-02 09:13:10 -0400

    added dependencies

--- models/shoppingList.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var List = sequelize.define("List", {
-        text: DataTypes.STRING,
-        complete: DataTypes.BOOLEAN
-    });
-    return List;
-};
